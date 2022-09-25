1130485b4c99bdfbc1c601d6cda977756bbe0852
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-09-25 20:42:05 +0700

    add constuctor and decontrucstor -like matrix prototype

--- shirotuljabarlib.h
+++ shirotuljabarlib.h
@@ -5,3 +5,12 @@ typedef struct shirotuljabar_mat_s {
     int is_square;
 
 } shirotuljabar_mat;
+
+// Constructor-like 
+// Allocates memory for a new matrix
+// All elements in the matrix are 0.0
+shirotuljabar_mat *shirotul_jabar_new(unsigned int num_rows, unsigned int num_cols);
+
+// Destructor-like
+// De-allocates the memory for the matrix
+void shirotuljabar_mat_free(shirotuljabar_mat *matrix);
