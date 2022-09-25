9b4d0e1eac7c6ab01c570a0bffa6ecdfe33b5a0d
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-09-25 21:23:57 +0700

    implement shirotuljabar_mat_new

--- shirotuljabarlib.c
+++ shirotuljabarlib.c
@@ -19,4 +19,26 @@ shirotuljabar_mat *shirotul_jabar_new(unsigned int num_rows, unsigned int num_co
         printf( "%s", "NULL_POINTER\n" );
         exit(-1);
     }
+
+    m->num_rows = num_rows;
+    m->is_square = num_cols;
+    m->is_square = (num_rows == num_cols) ? 1 : 0;
+    m->data = calloc(m->num_rows, sizeof(*m->data));
+    if ( !(m->data) ) {
+        printf( "%s", "NULL_POINTER create row\n" );
+        exit(-1);
+    }
+
+    for( int i = 0; i < num_rows; i++ ) {
+        m->data[i] = calloc(m->num_cols, sizeof(**m->data));
+        if ( !(m->data[i]) ) {
+        printf( "%s", "NULL_POINTER create columns \n" );
+        exit(-1);
+        }
+    }
+    return m;
+}
+
+int main( void ){
+    return 0;
 }
