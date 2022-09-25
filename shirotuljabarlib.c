eff52d3d9afc891e0927439bdb65299733cd3197
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-09-25 21:05:05 +0700

    add null_pointer checker

--- shirotuljabarlib.c
+++ shirotuljabarlib.c
@@ -5,11 +5,18 @@
 
 shirotuljabar_mat *shirotul_jabar_new(unsigned int num_rows, unsigned int num_cols) {
     if (num_rows == 0) {
-        printf( "%s", "INVALID_ROWS" );
+        printf( "%s", "INVALID_ROWS\n" );
         return NULL;
     }
     if (num_cols == 0) {
-        printf( "%s", "INVALID_COLS" );
+        printf( "%s", "INVALID_COLS\n" );
         return NULL;
+    }
+
+    shirotuljabar_mat *m = calloc(1, sizeof(*m));
+
+    if ( !(m) ) {
+        printf( "%s", "NULL_POINTER\n" );
+        exit(-1);
     }
 }
